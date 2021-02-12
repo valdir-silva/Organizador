6252cf8b48820630ec249ab26e0ef4fd3b68143c
Valdir Silva <valdir.silva@fusiondms.com.br>
2021-02-12 09:14:57 -0300

    Criando Intro

--- app/src/main/java/net/alunando/organizador/MainActivity.kt
+++ app/src/main/java/net/alunando/organizador/MainActivity.kt
@@ -2,10 +2,34 @@ package net.alunando.organizador
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.heinrichreimersoftware.materialintro.app.IntroActivity
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : IntroActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        isButtonBackVisible = false
+        isButtonNextVisible = false
+
+        addSlide( FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_1)
+                .build())
+
+        addSlide( FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_2)
+                .build())
+
+        addSlide( FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_3)
+                .build())
+
+        addSlide( FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_4)
+                .build())
     }
 }
