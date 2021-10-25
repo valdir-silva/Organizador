09c1625f00f2f72ebb36260fcbc1408a0603e458
valdir-silva <whiteofdark@outlook.com>
2021-10-24 22:56:19 -0300

    criando FloatingActionMenu

--- app/src/main/java/net/alunando/organizador/Principalctivity.kt
+++ app/src/main/java/net/alunando/organizador/Principalctivity.kt
@@ -1,6 +1,7 @@
 package net.alunando.organizador
 
 import android.os.Bundle
+import android.view.View
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -12,9 +13,17 @@ class Principalctivity : AppCompatActivity() {
         setContentView(R.layout.activity_principalctivity)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+//        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                    .setAction("Action", null).show()
+//        }
+    }
+
+    private fun adicionarDespesa(view: View) {
+
+    }
+
+    private fun adicionarReceita(view: View) {
+
     }
 }
